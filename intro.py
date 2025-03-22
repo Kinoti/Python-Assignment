a4f814fdada85f91f33d4ca533ee627cbe86f100
Kinoti <kinotimugambi99@gmail.com>
2025-03-22 16:03:32 +0300

    Initial commit

---
+++ intro.py
@@ -0,0 +1,19 @@
+#integer (whole number)
+age = 25
+#float (decimal number)
+height = 5.9
+#string (text)
+name = "Sammy"
+#boolean (True/False)
+is_student = True
+# check data types
+print(type(age)) #Output: <class 'int'>
+print(type(height)) #Output: <class 'int'>
+print(type(name)) #Output: <class 'int'>
+print(type(is_student)) #Output: <class 'int'>
+
+#Dynamic typing:
+age = 20
+print(age) # output : 20
+age = "Now I'm twenty"
+print(age) #output : string
